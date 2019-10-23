8d6c4b5dc50f8d90afcb2550e79718ef6b55c211
Arti Zirk <arti.zirk@gmail.com>
2019-10-23 23:22:16 +0300

    more opto

--- vhdl_kodutoo/fs1_opt1o.vhd
+++ vhdl_kodutoo/fs1_opt1o.vhd
@@ -11,31 +11,39 @@
 
 library IEEE; use IEEE.std_logic_1164.all;
 architecture opti of f_system is
-  signal x1i, x2i, x3i, x4i, t1, t2, t3, t4, t5: std_logic;
-  signal t6, t7, t8, t9, t10, t11, t12: std_logic;
+  signal x1i, x2i, x3i, x4i, y31, y4i, t1, t2, t3, t4, t5, l1: std_logic;
+  signal t6, t7, t8, t9, t10, t11, t12, t13, t61, t62, t71: std_logic;
 begin
   x1i <= not x1;
   x2i <= not x2;
   x3i <= not x3;
   x4i <= not x4;
 
-  t1 <= x1 and x2i;
-  t2 <= x3i and x4;
-  t3 <= x1i and x2;
-  y1 <= not (t1 or t2 or t3);
+  l1 <= x1 and x2;
 
-  t4 <= x2i and x4;
-  t5 <= x1i and x2;
-  y2 <= t4 or t5;
+  t1 <= x1 nand x2i;
+  t2 <= x3i nand x4;
+  t3 <= x1i nand x2;
+  y1 <= t1 and t2 and t3;
 
-  t6 <= x1i and x2 and x3i and x4;
-  t7 <= x1i and x3 and x4i;
-  t8 <= x1 and x2 and x3;
-  t9 <= x2i and x3i and x4i;
-  y3 <= not (t6 or t7 or t8 or t9);
+  t4 <= x2i nand x4;
+  t5 <= x1i nand x2;
+  y2 <= t4 nand t5;
 
-  t10 <= x1 and x2 and x3i and x4i;
-  t11 <= x1i and x3;
-  t12 <= x2i and x4;
-  y4 <= t10 or t11 or t12;
+  t61 <= x1 nor x3;
+  t62 <= x2 and x4;
+  t6  <= t61 nand t62;
+  t71 <= x1 nor x4;
+  t7 <= t71 nand x3;
+  t8 <= l1 nand x3;
+  t9 <= x2 or x3 or x4;
+  y31 <= t6 and t7 and t8;
+  y3 <= y31 and t9;
+
+  t13 <= x3 nor x4;
+  t10 <= l1 nand t13;
+  t11 <= x1i nand x3;
+  t12 <= x2i nand x4;
+  y4i <= t10 and t11 and t12;
+  y4 <= not y4i;
 end architecture opti;
